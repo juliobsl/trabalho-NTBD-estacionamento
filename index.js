7887216697e809841a1859b7a8388d3498dfeeac
juliobsl <julio@agenciafrato.com.br>
2022-05-09 23:41:58 -0300

    Carregando clintes e adicionando carros no pátio

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ app.get('/patio', (req, res) => {
     console.log('Listando pátio...');
     MongoClient.connect(url, function(err, db) {
         if (err) throw err;
-          var dbo = db.db("estacionamento");
-          dbo.collection("patio").find({}).toArray(function(err, result) {
+          var dbo = db.db("patio");
+          dbo.collection("controle").find({}).toArray(function(err, result) {
               if (err) throw err;
               //console.log(result);
               res.send(result);     
@@ -46,8 +46,24 @@ app.get('/patio', (req, res) => {
       });
 });
 
-app.post('/add_car_patio',(request,response) => {   
-    console.log(request.body.idcarro);
+app.post('/add_car_patio',(request,response) => { 
+    console.log("Adicionando carro #",request.body.codigocar," no pátio");
+    let date_ob = new Date();
+    let date = ("0" + date_ob.getDate()).slice(-2);
+    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+    let hours = date_ob.getHours();
+    let minutes = date_ob.getMinutes();
+    let year = date_ob.getFullYear();
+
+    var registro = {tipo: 'entrada', data: date+"/"+month+"/"+year, hora: hours + ":" + minutes ,codigo: request.body.codigocar,placa: request.body.placa, modelo: request.body.modelo, idcliente: request.body.codigocar};
+    MongoClient.connect(url, function(err, db) {
+        var dbo = db.db("patio");
+        dbo.collection("controle").insertOne(registro, function(err, res) {
+            if (err) throw res;
+            console.log("Carro inserido");
+            db.close();
+        });
+    });
 });
 app.post('/rm_car_patio',(request,response) => {   
     console.log(request.body.idcarro);
